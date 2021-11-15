c895e5b8ac5a73100010a0c266d17f8f3b41248b
username <username@users.noreply.github.com>
2021-11-15 17:51:54 -0500

    DB connection string and spanish language removal

--- P3AddNewFunctionalityDotNetCore/Data/P3Referential.cs
+++ P3AddNewFunctionalityDotNetCore/Data/P3Referential.cs
@@ -15,15 +15,15 @@ namespace P3AddNewFunctionalityDotNetCore.Data
         public virtual DbSet<Product> Product { get; set; }
 
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                //optionsBuilder.UseSqlServer(
-                //    "Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                //optionsBuilder.UseSqlServer(
+//                //    );
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- P3AddNewFunctionalityDotNetCore/Models/AppIdentityDbContext.cs
+++ P3AddNewFunctionalityDotNetCore/Models/AppIdentityDbContext.cs
@@ -9,15 +9,14 @@ namespace P3AddNewFunctionalityDotNetCore.Models
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
         : base(options) { }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(
-                    "Server=.;Database=Identity;Trusted_Connection=True;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer();
+//            }
+//        }
     }
 
 
